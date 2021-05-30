795c398d919053ddb7558224d78d382ffb8341cc
Suria narayanan Ramasamy <sramasam@u81z01s03.42kl.edu.my>
2021-05-30 20:06:02 +0800

    adjust spacing and tab

--- rush/random_num.c
+++ rush/random_num.c
@@ -1,22 +1,20 @@
 #include<stdlib.h>
 
-int random_num(void)
+int	random_num (void)
 {
-  int *p;
-  int random;
+	int	*p;
+	int	random;
 
-  p = (int*)malloc(sizeof(int)); 
-
-  random = ((int)p%5);
-
-  if (random == 0)
-  {
-    random += 1;
-  }
-  else if (random == 5)
-  {
-    random -= 1;
-  }
-  free(p);
-  return (random);
+	p = (int *) malloc (sizeof(int));
+	random = ((int)p % 5);
+	if (random == 0)
+	{
+		random += 1;
+	}
+	else if (random == 5)
+	{
+		random -= 1;
+	}
+	free(p);
+	return (random);
 }
