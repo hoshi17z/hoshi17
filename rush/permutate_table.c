2cb76e0e0b88c56edeb2ccb14a7c512e66c021e7
Suria narayanan Ramasamy <sramasam@u81z01s03.42kl.edu.my>
2021-05-30 19:57:43 +0800

    adjust spacing and tab

--- rush/permutate_table.c
+++ rush/permutate_table.c
@@ -1,18 +1,17 @@
 int	random_num(void);
 
-char *permutate_table(int *box, int row)
+char	*permutate_table (int *box, int row)
 {
-    int sum;
+	int	sum;
 
-    sum = box[row][1] + box[row][2] + box[row][3] + box[row][4];
-    while (sum != 10)
-    {
-        box[row][1] = random_num();
-        box[row][2] = random_num();
-        box[row][3] = random_num();
-        box[row][4] = random_num();
-
-        sum = box[row][1] + box[row][2] + box[row][3] + box[row][4];
-    }
-    return (box);
+	sum = box[row][1] + box[row][2] + box[row][3] + box[row][4];
+	while (sum != 10)
+	{
+		box[row][1] = random_num();
+		box[row][2] = random_num();
+		box[row][3] = random_num();
+		box[row][4] = random_num();
+		sum = box[row][1] + box[row][2] + box[row][3] + box[row][4];
+	}
+	return (box);
 }
